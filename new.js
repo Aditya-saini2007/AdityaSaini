45f6b63437d39daeab633c531e8ba46ede015a63
Aditya-saini2007 <aditya.ay0014@gmail.com>
2024-12-01 15:44:12 +0530

    new

--- new.js
+++
@@ -1,7 +0,0 @@
-//js one shot from starting
-//console.log("xyz"); used to print anything.
-// ----------------------------
-//variable in js
-// types of variables are there - let , var , const.
-//-----------------------------
-//
