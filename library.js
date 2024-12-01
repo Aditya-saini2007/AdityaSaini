2eec434df27d8a99d4322e710532be14da06dc36
Aditya-saini2007 <aditya.ay0014@gmail.com>
2024-12-01 17:30:58 +0530

    hdbjhwcb

--- library.js
+++ library.js
@@ -1,3 +1,38 @@
 class Library{
-    
+    constructor(LibraryName,LibraryAddress,owner,estYear){
+        this.LibraryName = LibraryName;
+        this.LibraryAddress = LibraryAddress;
+        this.owner = owner;
+        this.ListOfBooks = []
+        this.estYear = estYear;
+        this.ListOfUsers =[]
+        this.borrowerDetail = []
+        this.usercount = 0;
+    }
+    addstock(bTitle,bAuthor,bQty,bPrice,bYear,bType){
+        this.ListOfBooks.push(new BookStock(bTitle,bAuthor,bQty,bPrice,bYear,bType))
+    }
+    issuebook(){
+
+    }
+    returnbook(){
+
+    }
+    adduser(name,){
+
+    }
+}
+
+
+class BookStock{
+    constructor(){
+
+    }
+}
+
+
+class UserDetails{
+    constructor(){
+        
+    }
 }
